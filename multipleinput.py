508113365f5e2356cfd6d489f0f920b5096fff47
Tanbeer Jubaer <85180135+oitik@users.noreply.github.com>
2023-03-08 14:59:13 +0600

    Update multipleinput.py
    i/o added

--- multipleinput.py
+++ multipleinput.py
@@ -1,2 +1,7 @@
 inp = [input().split() for i in range(int(input()))]
 print(inp)
+# Example --> input-->
+# 2
+# Oitik jubaer 32 M
+# Abrar zahin 21 M
+# output --> [['Oitik', 'jubaer', '32', 'M'], ['Abrar', 'zahin', '21', 'M']]
